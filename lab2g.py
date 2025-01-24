75ea70a905470c43212f0227bb4cccf1d06308cb
Prashant KC <pk-c2@myseneca.ca>
2025-01-24 16:27:45 -0500

    Initial commit

---
+++ lab2g.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Your Name
+# Author ID: Your Seneca ID
+# Date Created: yyyy/mm/dd
+import sys
+
+if len(sys.argv) > 1:
+    timer = int(sys.argv[1])
+else:
+    timer = 3
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
